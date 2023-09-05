1c6455b3e589f2bd8171f5c0e14b5c85484a3ed0
Ashwini Sharma <90304035+Xordy-Codes@users.noreply.github.com>
2023-09-05 20:21:04 +0530

    Update script.js

--- Social Media Website/script.js
+++ Social Media Website/script.js
@@ -12,6 +12,7 @@ const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customize-theme');
 const fontSizes = document.querySelector('.choose-size span');
 var root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
 
 //remove active class  from all menu items
     const changeActiveItem = () => {
@@ -80,10 +81,10 @@ const openThemeModal = () => {
 themeModal.addEventListener('click', closeThemeModal)
 theme.addEventListener('click', openThemeModal);
 
-//-----------------------------fonts---------------------
-fontSizes.forEach(size => 
-    {
+//-----------------------------fonts----------------------------
+fontSizes.forEach(size => {
     let fontSize;
+    
 
     size.addEventListener('click', () => {
         if(size.classList.contain('font-size-1')){
@@ -105,12 +106,36 @@ fontSizes.forEach(size =>
         }else if(size.classList.contain('font-size-5')){
             fontSize = '22px';
             root.style.setProperty('----sticky-top-left', '-10rem');
-            root.style.setProperty('----sticky-top-right', '-33rem');
+            root.style.setProperty('----sticky-top-right', '-35rem');
         }
         document.querySelector('html').style.fontSize = fontSize;
     })
-
-    // CHANGE font size
-    
 })
 
+
+//remove active class from colors
+
+const changeActiveColorClass = () => {
+
+}
+    // change primary color
+    colorPalette.forEach(color => {
+        color.addEventListener('click', () => {
+            let primaryHue;
+    
+            if (color.classList.contains('color-1')) {
+                primaryHue = 252;
+            } else if (color.classList.contains('color-2')) {
+                primaryHue = 52;
+            } else if (color.classList.contains('color-3')) {
+                primaryHue = 352;
+            } else if (color.classList.contains('color-4')) {
+                primaryHue = 152;
+            } else if (color.classList.contains('color-5')) {
+                primaryHue = 202;
+            }
+            color.classList.add('active');
+    
+            root.style.setProperty('--color-primary', primaryHue);
+        });
+    });
