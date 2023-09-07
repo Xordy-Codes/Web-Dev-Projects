4b1edf70f542e39e1fa24b13a565b009fc9d7fdf
Ashwini Sharma <90304035+Xordy-Codes@users.noreply.github.com>
2023-09-08 02:05:19 +0530

    Update script.js

--- Social Media Website/script.js
+++ Social Media Website/script.js
@@ -4,15 +4,18 @@ const menuItems = document.querySelectorAll('.menu-item');
 //Messages
 const messagesNotification = document.querySelector('#messages-notification');
 const messages = document.querySelector('.messages');
-const message = messages.querySelectorAll('.message');
+const message = messages.querySelectorAll('.messages');
 const messageSearch = document.querySelector('#message-search');
 
 //theme
 const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customize-theme');
-const fontSizes = document.querySelector('.choose-size span');
+const fontSizes = document.querySelectorAll('.choose-size span');
 var root = document.querySelector(':root');
 const colorPalette = document.querySelectorAll('.choose-color span');
+const Bg1 = document.querySelector('.bg-1');
+const Bg2 = document.querySelector('.bg-2');
+const Bg3 = document.querySelector('.bg-3');
 
 //remove active class  from all menu items
     const changeActiveItem = () => {
@@ -81,31 +84,42 @@ const openThemeModal = () => {
 themeModal.addEventListener('click', closeThemeModal)
 theme.addEventListener('click', openThemeModal);
 
-//-----------------------------fonts----------------------------
-fontSizes.forEach(size => {
-    let fontSize;
-    
 
+
+//-----------------------------fonts----------------------------
+
+const removeSizeSelector = () => {
+    fontSizes.forEach(size => {
+        size.classList.remove('active');
+    })
+}
+
+fontSizes.forEach(size => {
     size.addEventListener('click', () => {
-        if(size.classList.contain('font-size-1')){
+
+        removeSizeSelector();
+        let fontSize;
+        size.classList.toggle('active');
+
+        if(size.classList.contains('font-size-1')){
             fontSize = '10px';
             root.style.setProperty('----sticky-top-left', '5.4rem');
             root.style.setProperty('----sticky-top-right', '5.4rem');
-        }else if(size.classList.contain('font-size-2')){
+        }else if(size.classList.contains('font-size-2')){
             fontSize = '13px';
             root.style.setProperty('----sticky-top-left', '5.4rem');
             root.style.setProperty('----sticky-top-right', '-7rem');
-        }else if(size.classList.contain('font-size-3')){
+        }else if(size.classList.contains('font-size-3')){
             fontSize = '16px';
             root.style.setProperty('----sticky-top-left', '-2rem');
             root.style.setProperty('----sticky-top-right', '-17rem');
-        }else if(size.classList.contain('font-size-4')){
+        }else if(size.classList.contains('font-size-4')){
             fontSize = '19px';
             root.style.setProperty('----sticky-top-left', '-5rem');
             root.style.setProperty('----sticky-top-right', '-25rem');
-        }else if(size.classList.contain('font-size-5')){
+        }else if(size.classList.contains('font-size-5')){
             fontSize = '22px';
-            root.style.setProperty('----sticky-top-left', '-10rem');
+            root.style.setProperty('----sticky-top-left', '-12rem');
             root.style.setProperty('----sticky-top-right', '-35rem');
         }
         document.querySelector('html').style.fontSize = fontSize;
@@ -116,15 +130,24 @@ fontSizes.forEach(size => {
 //remove active class from colors
 
 const changeActiveColorClass = () => {
-
+colorPalette.forEach(colorPicker => {
+    colorPicker.classList.remove('active');
+})
 }
-    // change primary color
+
+
+
+    //-------------------------------change primary color--------------------------------
+
     colorPalette.forEach(color => {
         color.addEventListener('click', () => {
             let primaryHue;
-    
+
+            //remove active class from colors
+            changeActiveColorClass();
+            
             if (color.classList.contains('color-1')) {
-                primaryHue = 252;
+                primaryHue = 252;                                                                                 
             } else if (color.classList.contains('color-2')) {
                 primaryHue = 52;
             } else if (color.classList.contains('color-3')) {
@@ -136,6 +159,60 @@ const changeActiveColorClass = () => {
             }
             color.classList.add('active');
     
-            root.style.setProperty('--color-primary', primaryHue);
-        });
-    });
+            root.style.setProperty('--primary-color-hue', primaryHue);
+        })
+    })
+
+    //theme background values
+    let lightColorLightness;
+    let whiteColorLightness;
+    let darkColorLightness;
+
+    const changeBG = () => {
+        root.style.setProperty('--light-color-lightness', lightColorLightness);
+        root.style.setProperty('--white-color-lightness', whiteColorLightness);
+        root.style.setProperty('--dark-color-lightness', darkColorLightness);
+    }
+
+    Bg1.addEventListener('click', () => {
+        lightColorLightness = '15%';
+        whiteColorLightness = '20%';
+        darkColorLightness = '95%';
+
+        //add active class
+        Bg1.classList.add('active');
+        //remove active class from the others
+        Bg2.classList.remove('active');
+        Bg3.classList.remove('active');
+        window.location.reload();
+    })
+
+    Bg2.addEventListener('click', () => {
+        lightColorLightness = '15%';
+        whiteColorLightness = '20%';
+        darkColorLightness = '95%';
+
+        //add active class
+        Bg2.classList.add('active');
+        //remove active class from the others
+        Bg1.classList.remove('active');
+        Bg3.classList.remove('active');
+        changeBG();
+    })
+
+    Bg3.addEventListener('click', () => {
+        lightColorLightness = '0%';
+        whiteColorLightness = '10%';
+        darkColorLightness = '95%';
+
+        //add active class
+
+        Bg3.classList.add('active');
+        //remove active class from the others
+        Bg1.classList.remove('active');
+        Bg2.classList.remove('active');
+        changeBG();
+    })
+    
+
+    
